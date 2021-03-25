5af8d64632e4272e85bcd83ed2e10f2c7381f5c9
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-24 23:03:49 -0400

    Part 8, 8.10

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ALL_BOOKS } from '../queries';
 import { useQuery } from '@apollo/client';
 
--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import { ALL_BOOKS, ALL_AUTHORS, CREATE_BOOK } from '../queries';
+import { useMutation } from '@apollo/client';
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [ createBook, result ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ]
+  });
 
   if (!props.show) {
     return null
@@ -13,12 +19,14 @@ const NewBook = (props) => {
 
   const submit = async (event) => {
     event.preventDefault()
-    
+
     console.log('add book...')
+
+    createBook({ variables: { title, published: Number(published), author, genres } });
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +50,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
